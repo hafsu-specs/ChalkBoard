0afc23846f7efe88b54c312f373261715404bf2d
Rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2021-12-05 00:56:51 -0500

    created MySQL database on linux, accounts table contains all user information, login form now authticating students before letting them in the student home page

--- server.js
+++ server.js
@@ -1,41 +1,72 @@
 // Load Node modules
 var express = require('express');
 const ejs = require('ejs');
-const dotenv = require('dotenv');
-const bodyParser = require("body-parser");
-const user = require("./routes/user"); //new addition
-const InitiateMongoServer = require("./config/db");
+var path = require('path');
+var mysql = require('mysql');
+var session = require('express-session');
+var bodyParser = require("body-parser");
 
-// Initiate Mongo Server
-InitiateMongoServer();
-//require env
-dotenv.config({path:'./config/config.env'})
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static('public'));
 // Middleware
 app.use(bodyParser.json());
-/**
- * Router Middleware
- * Router - /user/*
- * Method - *
- */
-app.use("/user", user);
-// Set the view engine to ejs
+
 app.set('view engine', 'ejs');
 // Port website will run on
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server is running on port: ${PORT}` ));
 
+//connect with the mysql database on linux
+var connection = mysql.createConnection({
+	host     : 'mars.cs.qc.cuny.edu',
+	user     : 'agru2717',
+	password : '23722717',
+	database : 'agru2717'
+});
+
+//using express packages
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
 //tracks all activities
 var listnames=[];
 var i=0;
-// *** GET Routes - display pages ***
-// Root Route
-app.get('/json', (req, res) => {
-res.status(200).json({success: true, msg: "Show all users"});
+
+//When the client enters their details in the login form and clicks the submit button, the form data will be sent to the server
+// and with that data our login script will check in our MySQL accounts table to see if the details are correct
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/StudentHome');
+			} else {
+                //response.send("incorrect username/or password");
+                listnames[i]=("user redirected to home page: login failed");
+                i++;
+                response.redirect('/');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 });
+
+// *** GET Routes - display pages ***
+
+//landing page
 app.get('/', function (req, res) {
     listnames[i] = ("Index page opened");
     i++;
@@ -51,28 +82,52 @@ app.get('/SignUp', function (req, res) {
     res.render('pages/SignUp');
 });
 
-app.post('/SignUp', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            username: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-            re_password: hashedPassword
-        })
-        res.redirect('/')
-    } catch {
-        res.redirect('/SignUp')
-    }
-})
-
-
-app.get('/StudentHome', function (req, res) {
-    listnames[i]=("student logged in");
+app.get('/temp', function (req, res) {
+    listnames[i] = ("Temporary page with links to all views");
     i++;
-    res.render('pages/StudentCoursesHomePages');
+    res.render('pages/temp');
 });
+
+app.get('/StudentProfile', function (request, response) {
+    if (request.session.loggedin) {
+        listnames[i]=(request.session.username + " student profile");
+        i++;
+        response.render('pages/StudentProfile');
+       return;
+   } else {
+        listnames[i]=("student access denied: please login");
+        i++;
+        response.redirect('/');
+   }
+   response.end();
+});
+
+app.get('/SignUp', function (req, res) {
+    listnames[i] = ("Signup opened");
+    i++;
+    res.render('pages/SignUp');
+});
+
+app.get('/About', function (req, res) {
+    listnames[i]=("Opened About page");
+    i++;
+    res.render('pages/About');
+});
+
+app.get('/StudentHome', function(request, response) {
+	if (request.session.loggedin) {
+         listnames[i]=(request.session.username + " student logged in");
+         i++;
+         response.render('pages/StudentCoursesHomePage');
+        return;
+	} else {
+         listnames[i]=("student access denied: please login");
+         i++;
+         response.redirect('/');
+	}
+	response.end();
+});
+
 app.get('/InstructorHome', function (req, res) {
     listnames[i] = ("instructor logged in our app");
     i++;
@@ -86,22 +141,3 @@ app.get('/AdminView', function (req, res) {
         listnames: listnames
     });
 });
-
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/')
-})
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-    res.redirect('/')
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
-    }
-    next()
-}
