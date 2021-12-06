4e6820c88312d37970a70e1de71ce11f941962d5
Rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2021-12-06 16:22:30 -0500

    made consistant nav bar, connected pages together, added create a drop course feature to instructor

--- server.js
+++ server.js
@@ -208,7 +208,8 @@ app.get('/AdminView', function(request, response) {
          listnames[i++]=(request.session.username + ": Admin logged in");
          response.render('pages/AdminView', {
             // EJS variable and server-side variable
-            listnames: listnames
+            listnames: listnames,
+            i: "#"
         });
         return;
 	} else {
@@ -217,3 +218,19 @@ app.get('/AdminView', function(request, response) {
 	}
 	response.end();
 });
+
+//logout
+// DELETE /api/auth/logout
+// router.delete('/logout', (req, res) => {
+//     if (req.session) {
+//       req.session.destroy(err => {
+//         if (err) {
+//           res.status(400).send('Unable to log out')
+//         } else {
+//           res.send('Logout successful')
+//         }
+//       });
+//     } else {
+//       res.end()
+//     }
+//   })
